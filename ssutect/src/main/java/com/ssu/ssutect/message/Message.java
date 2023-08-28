dc4b8a1aa8b72fbdd4ee381e2c73a71aead9b23d
lovee9523 <noeun1102@gmail,com>
2023-08-28 15:19:04 +0900

    Message 엔티티 수정

--- ssutect/src/main/java/com/ssu/ssutect/message/Message.java
+++ ssutect/src/main/java/com/ssu/ssutect/message/Message.java
@@ -1,15 +1,15 @@
 package com.ssu.ssutect.message;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity @Getter
+@Getter @Setter
 public class Message {
 
-    @Id @GeneratedValue
     private Long id;
 
     private Long count; // 누적 횟수
