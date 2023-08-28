3f81050ad4a7172c653a7bf42cf6d9eadd1c481e
lovee9523 <noeun1102@gmail,com>
2023-08-28 16:25:27 +0900

    apiMapper.xml 에 전체 메시지 조회 mapper 추가

--- ssutect/src/main/java/com/ssu/ssutect/message/repository/MessageRepository.java
+++ ssutect/src/main/java/com/ssu/ssutect/message/repository/MessageRepository.java
@@ -1,7 +1,14 @@
 package com.ssu.ssutect.message.repository;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
+@Repository
 public interface MessageRepository {
+
+    List<String> getAllMessage();
+
 }
