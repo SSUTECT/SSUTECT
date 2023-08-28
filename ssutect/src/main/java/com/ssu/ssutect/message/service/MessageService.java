f1cc2ed85527805846e9bb5f16bb20d149016a39
lovee9523 <noeun1102@gmail,com>
2023-08-28 16:44:23 +0900

    MessageService에 compareMessages() 함수 추가

--- ssutect/src/main/java/com/ssu/ssutect/message/service/MessageService.java
+++ ssutect/src/main/java/com/ssu/ssutect/message/service/MessageService.java
@@ -17,4 +17,23 @@ public class MessageService {
         return messages;
     }
 
+    public Boolean compareMessages(String userMessage) {
+        List<String> allMessage = getAllMessage();
+        for (String m : allMessage) {
+            // ai를 이용한 비교 코드 작성
+            Boolean pac = compareMessage(m, userMessage);
+            if(pac == true){
+                return true; // 스팸 메시지이면 true 반환
+            }
+        }
+        return false; // 스팸 메시지가 아니면 false 리턴
+    }
+
+    public Boolean compareMessage(String m1, String m2) {
+        if(m1.equals(m2))
+            return true;
+
+        return false;
+    }
+
 }
