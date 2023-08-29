1748bcbcb27a89c8cca83e8d7a4fcb01ef3b57d0
leehw1011 <leehw1011@naver.com>
2023-08-29 14:25:38 +0900

    MessageService : processMessageReport 함수 추가

--- ssutect/src/main/java/com/ssu/ssutect/message/service/MessageService.java
+++ ssutect/src/main/java/com/ssu/ssutect/message/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.ssu.ssutect.message.service;
 
+import com.ssu.ssutect.message.Message;
 import com.ssu.ssutect.message.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -36,4 +37,18 @@ public class MessageService {
         return false;
     }
 
+    public Boolean processMessageReport(String content){
+        Message message = messageRepository.findByContent(content);
+        if(message != null){    // 이미 같은 내용을 갖는 문자가 DB에 존재하면
+            message.setCount(message.getCount() + 1);   // 신고 횟수를 증가시킴
+            messageRepository.save(message);
+        }
+        else {
+            Message reportMessage = new Message();
+            reportMessage.setContent(content);
+            reportMessage.setCount(1L);
+            reportMessage.setReport(1L);     // report 1 : 제보된 메세지 / report 0 : 기존 저장된 메세지
+            messageRepository.save(reportMessage);
+        }
+    }
 }
